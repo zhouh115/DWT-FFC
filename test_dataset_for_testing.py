69ec3c0c0e70e500ae9ac2338ba2a6b2611f22e6
zhouh115 <128397358+zhouh115@users.noreply.github.com>
2024-01-15 13:52:40 -0500

    Update test_dataset_for_testing.py

--- test_dataset_for_testing.py
+++ test_dataset_for_testing.py
@@ -6,16 +6,25 @@ import os
 class dehaze_test_dataset(Dataset):
     def __init__(self, test_dir):
         self.transform = transforms.Compose([transforms.ToTensor()])
-        self.list_test=[]
-        for i in os.listdir(test_dir):
-            self.list_test.append(i)
-        self.root_hazy = os.path.join(test_dir)
-        self.file_len = len(self.list_test)
+        self.list_test_hazy=[]
+
+        self.root_hazy=os.path.join(test_dir, 'hazy/')
+        for i in os.listdir(self.root_hazy):
+           self.list_test_hazy.append(i)
+        #self.root_hazy = os.path.join(test_dir)
+
+        self.list_test_clean=[]
+        self.root_clean=os.path.join(test_dir, 'clean/')
+        for j in os.listdir(self.root_clean):
+           self.list_test_clean.append(j)
+
+
+        self.file_len = len(self.list_test_hazy)
+
+
     def __getitem__(self, index, is_train=True):
-        hazy = Image.open(self.root_hazy + self.list_test[index]).convert('RGB')
-        #print(hazy)
+        hazy = Image.open(self.root_hazy + self.list_test_hazy[index])
         hazy = self.transform(hazy)
-        #print(hazy.shape)
         if hazy.shape[1]<hazy.shape[2]:
             hazy_up_left=hazy[:,0:1600, 0:2432]
             hazy_up_middle=hazy[:, 0:1600, 1800:4232]
@@ -29,7 +38,7 @@ class dehaze_test_dataset(Dataset):
             hazy_down_middle=hazy[:, 2400:4000, 1800:4232]
             hazy_down_right=hazy[:,2400:4000, 3568:6000]
 
-            name=self.list_test[index]
+            name=self.list_test_hazy[index]
 
 
         if hazy.shape[1]>hazy.shape[2]:
@@ -45,11 +54,12 @@ class dehaze_test_dataset(Dataset):
             hazy_down_middle=hazy[:, 3568:6000, 1200:2800]
             hazy_down_right=hazy[:,3568:6000, 2400:]
 
+            name=self.list_test_hazy[index]
 
-
-
-            name=self.list_test[index]
-
-        return hazy_up_left, hazy_up_middle, hazy_up_right, hazy_middle_left, hazy_middle_middle, hazy_middle_right, hazy_down_left, hazy_down_middle, hazy_down_right, name
+        clean=Image.open(self.root_clean + self.list_test_clean[index])
+        clean = self.transform(clean)
+        
+        return hazy_up_left, hazy_up_middle, hazy_up_right, hazy_middle_left, hazy_middle_middle, hazy_middle_right, hazy_down_left, hazy_down_middle, hazy_down_right, name, clean
     def __len__(self):
         return self.file_len
+
